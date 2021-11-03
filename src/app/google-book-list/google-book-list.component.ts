d040fab49f19eef34a522977e5f9ff4d1ef11e31
Zhixian Ong <zhixian@hotmail.com>
2021-11-03 17:55:35 +0800

    add list component

---
+++ src/app/google-book-list/google-book-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import { Book } from '../models/book';
+
+@Component({
+  selector: 'app-google-book-list',
+  templateUrl: './google-book-list.component.html',
+  styleUrls: ['./google-book-list.component.css']
+})
+export class GoogleBookListComponent implements OnInit {
+
+  @Input() books: ReadonlyArray<Book> = [];
+  
+  @Output() add = new EventEmitter<string>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
