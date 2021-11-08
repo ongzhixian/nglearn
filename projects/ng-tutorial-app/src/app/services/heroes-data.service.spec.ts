ee6f761ee9b40e4f8d04b553f24f7c9042ecfdde
Zhixian Ong <zhixian@hotmail.com>
2021-11-08 12:08:44 +0800

    Add heroes in-memory data service

---
+++ projects/ng-tutorial-app/src/app/services/heroes-data.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { HeroesDataService } from './heroes-data.service';
+
+describe('HeroesDataService', () => {
+  let service: HeroesDataService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(HeroesDataService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
