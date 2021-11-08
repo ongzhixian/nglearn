8676dd20591effbab6b1b94878b80ba6288b713e
Zhixian Ong <zhixian@hotmail.com>
2021-11-08 17:13:03 +0800

    Add validators

--- projects/ng-tutorial-app/src/app/components/login/login.component.ts
+++ projects/ng-tutorial-app/src/app/components/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+
+import { FormGroup, FormControl, AbstractControl, Validators } from '@angular/forms'
+
 import { LogService } from '../../services/log.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,14 +12,77 @@ import { LogService } from '../../services/log.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private logService : LogService) { }
+  public loginError: string = "";
+
+  loginForm = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(3)])
+  });
+
+  constructor(
+    private router: Router,
+    private logService: LogService) { }
 
   ngOnInit(): void {
     this.logService.add("In Login OnInit");
   }
 
-  login() : void {
+  login(): void {
     this.logService.add("TODO: login actions.");
+  }
+
+  onSubmit(): void {
+
+    this.logService.add(`onSubmit() ==> Username: [${this.loginForm.value.username}], Password: [${this.loginForm.value.password}]`);
+
+    if (this.loginForm.valid) {
+      this.logService.add("Form is valid; check credentials next;");
+
+      // this.apiService.login(this.loginForm.value)
+      //   .subscribe((data) => {
+
+      //     console.log(data);
+      //     if (data.status === 200 && !data.body.ErrorCode) {
+      //       this.router.navigate(['/dashboard']);
+      //     } else {
+      //       this.loginError = data.body.message;
+      //     }
+      //   },
+      //     error => this.loginError = error
+      //   )
+    }
+    this.loginError = "Invalid form";
+
+
+    // this.router.navigate(["/dashboard"]);
+  }
+
+  getErrorMessage(formControl : AbstractControl) {
+
+    if (formControl.errors?.required) {
+      return "A valid is required for this field.";
+    }
+
+    if (formControl.errors?.minlength) {
+      return `This field needs to have a minimal of ${formControl.errors?.minlength.requiredLength} characters.`;
+    }
+
+    return "Invalid input";
+
+    
+    // return "yaya";
+    
+    // console.log(typeof  formControl);
+
+    // if (formControl.hasError('required')
+    
+    // debugger;
+
+    // if (this.loginForm.value.userName.hasError('required')) {
+    //   return 'You must enter a value';
+    // }
+
+    // return this.loginForm.value.hasError('email') ? 'Not a valid email' : '';
   }
 
 }
