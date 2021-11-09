1aed52e43dc13b28273e1dd1c6a25882977a775b
Zhixian Ong <zhixian@hotmail.com>
2021-11-09 16:32:51 +0800

    Add GoogleBooks service

---
+++ projects/ng-tutorial-app/src/app/services/google-books.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+import { catchError, tap, map } from 'rxjs/operators';
+import { GoogleBook } from '../models/google-book';
+import { LogService } from './log.service';
+import { mock } from '../models/mockBooks';
+
+@Injectable({ providedIn: 'root' })
+export class GoogleBooksService {
+  constructor(private http: HttpClient,
+    private log : LogService) {}
+
+  getBooks(): Observable<Array<GoogleBook>> {
+    return this.http
+      .get<{ items: GoogleBook[] }>('https://books.googleapis.com/books/v1/volumes?q=frank%20herbert')
+      .pipe(
+        tap(_ => this.log.add(`${_.items.length} items retrieved.`)),
+        map((books) => books.items || []),
+        catchError(this.handleError<GoogleBook[]>('Get books', []))
+      );
+
+    // return of(mock).pipe(map((books) => books.items || []));
+  }
+
+  // getBooks(): Observable<Array<GoogleBook>> {
+  //   return of(mock).pipe(map((books) => books.items || []));
+  // }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+  
+      // TODO: better job of transforming error for user consumption
+      this.log.add(`${operation} failed: ${error.message}`);
+  
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+}
