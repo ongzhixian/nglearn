5b61dce024c02ef8863b1d0fcd309fb9097bf2b3
Zhixian Ong <zhixian@hotmail.com>
2021-11-09 09:33:19 +0800

    add logout to routes

--- projects/ng-tutorial-app/src/app/modules/app-routing/app-routing.module.ts
+++ projects/ng-tutorial-app/src/app/modules/app-routing/app-routing.module.ts
@@ -5,15 +5,19 @@ import { DummyPage1Component } from '../../components/dummy-page1/dummy-page1.co
 import { DummyPage2Component } from '../../components/dummy-page2/dummy-page2.component';
 import { DummyPage3Component } from '../../components/dummy-page3/dummy-page3.component';
 import { LoginComponent } from '../../components/login/login.component';
+import { LogoutComponent } from '../../components/logout/logout.component';
+
+import { AuthenticatedUserGuard } from '../../guards/authenticated-user.guard';
 
 const routes : Routes = [
   
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'page1', component: DummyPage1Component },
-  { path: 'page2', component: DummyPage2Component },
-  { path: 'page3', component: DummyPage3Component },
-  { path: 'login', component: LoginComponent }
+  { path: 'page2', component: DummyPage2Component, canActivate: [AuthenticatedUserGuard] },
+  { path: 'page3', component: DummyPage3Component, canActivate: [AuthenticatedUserGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: 'logout', component: LogoutComponent },
   // { path: 'detail/:id', component: HeroDetailComponent },
   // { path: 'heroes', component: HeroesComponent }
   // { path: '**', redirectTo: '/' }
