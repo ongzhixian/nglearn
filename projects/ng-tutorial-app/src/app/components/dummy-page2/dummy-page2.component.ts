2f7a3b9d440de46d5ad3edaab0af0f090b0968c7
Zhixian Ong <zhixian@hotmail.com>
2021-11-09 16:54:34 +0800

    add google book list to dummy page2

--- projects/ng-tutorial-app/src/app/components/dummy-page2/dummy-page2.component.ts
+++ projects/ng-tutorial-app/src/app/components/dummy-page2/dummy-page2.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { LogService } from '../../services/log.service';
+import { Store } from '@ngrx/store';
+
+import { AppState } from '../../state/app.state';
+import { selectBookCollection, selectBooks } from '../../state/google-books.selector';
+import { retrievedBookList, addBook, removeBook } from '../../state/google-books.actions';
+import { GoogleBooksService } from '../../services/google-books.service';
 
 @Component({
   selector: 'app-dummy-page2',
@@ -8,10 +14,31 @@ import { LogService } from '../../services/log.service';
 })
 export class DummyPage2Component implements OnInit {
 
-  constructor(private logService : LogService) { }
+  books$ = this.store.select(selectBooks);
+  
+  bookCollection$ = this.store.select(selectBookCollection);
+
+  constructor(private logService : LogService,
+    private booksService: GoogleBooksService,
+    private store: Store<AppState>) { }
 
   ngOnInit(): void {
     this.logService.add("In DummyPage2Component OnInit");
+    this.booksService
+    .getBooks()
+    .subscribe(
+      (books) => {
+        this.store.dispatch(retrievedBookList({ books }));
+        // debugger;
+      }
+    );
   }
 
+  onAdd(bookId: string) {
+    this.store.dispatch(addBook({ bookId }));
+  }
+ 
+  onRemove(bookId: string) {
+    this.store.dispatch(removeBook({ bookId }));
+  }
 }
