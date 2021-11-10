0110268429595cd1d3297c817c840ebfbc91cb7a
Zhixian Ong <zhixian@hotmail.com>
2021-11-10 13:31:36 +0800

    test idea to add enumerate for actions

--- projects/ng-tutorial-app/src/app/state/authentication.actions.ts
+++ projects/ng-tutorial-app/src/app/state/authentication.actions.ts
@@ -1,13 +1,22 @@
 import { createAction, props } from '@ngrx/store';
 
+export enum AuthenticationActionTypes {
+    LOGIN = '[Login Component] Login',
+    LOGIN_SUCCESS = '[Login Component] Login Success',
+    LOGIN_FAILURE = '[Login Component] Login Failure',    
+    LOGOUT          = '[Logout Component] Logout',
+    LOGOUT_SUCCESS  = '[Logout Component] Logout Success',
+    LOGOUT_FAILURE  = '[Logout Component] Logout Failure'
+}
+
 export const login = createAction(
-    '[Login Component] Login',
-    props<{ 
-        username: string; 
-        password: string 
+    AuthenticationActionTypes.LOGIN,
+    props<{
+        username: string;
+        password: string
     }>()
 );
 
 export const logout = createAction(
-    '[Component] Logout'
+    AuthenticationActionTypes.LOGOUT
 );
