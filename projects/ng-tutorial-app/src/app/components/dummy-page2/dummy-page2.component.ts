2786b1c5f11ab8b5f8c138becaeba80fe5806698
Zhixian Ong <zhixian@hotmail.com>
2021-11-10 13:30:05 +0800

    change to use effects

--- projects/ng-tutorial-app/src/app/components/dummy-page2/dummy-page2.component.ts
+++ projects/ng-tutorial-app/src/app/components/dummy-page2/dummy-page2.component.ts
@@ -2,10 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { LogService } from '../../services/log.service';
 import { Store } from '@ngrx/store';
 
-// import { AppState } from '../../state/app.state';
+import { AppState } from '../../state/app.state';
 import { selectBookCollection, selectBooks } from '../../state/google-books.selector';
-import { retrievedBookList, addBook, removeBook } from '../../state/google-books.actions';
+import { retrievedBookList, addBook, removeBook, getBookList } from '../../state/google-books.actions';
 import { GoogleBooksService } from '../../services/google-books.service';
+import { Observable, of } from 'rxjs';
+import { GoogleBook } from '../../models/google-book';
 
 @Component({
   selector: 'app-dummy-page2',
@@ -14,25 +16,55 @@ import { GoogleBooksService } from '../../services/google-books.service';
 })
 export class DummyPage2Component implements OnInit {
 
-  books$ = this.store.select(selectBooks);
+  // books$ = this.store.select(selectBooks);
+  
+  books$! : Observable<GoogleBook[]>;
   
   bookCollection$ = this.store.select(selectBookCollection);
 
   constructor(private logService : LogService,
     private booksService: GoogleBooksService,
-    private store: Store
+    private store: Store<AppState>
     ) { }
 
   ngOnInit(): void {
     this.logService.add("In DummyPage2Component OnInit");
-    this.booksService
-    .getBooks()
-    .subscribe(
+    // this.booksService
+    // .getBooks()
+    // .subscribe(
+    //   (books) => {
+    //     this.store.dispatch(retrievedBookList({ books }));
+    //     // debugger;
+    //   }
+    // );
+
+    // this.store.pipe(select(storeSelectors.selectAllCakes)).subscribe(data => {
+    //   this.allCakes = data;
+    // });
+
+    // this.store$.select(BookStoreSelectors.getBooks).subscribe(
+    //     books => 
+    //     {
+    //         this.books$ = of(this.books);
+    //     }
+    // );
+
+    this.store.select(selectBooks).subscribe(
       (books) => {
-        this.store.dispatch(retrievedBookList({ books }));
-        // debugger;
+        this.logService.add(`In selectBooks subscribe ${books}`)
+        this.books$ = of(books)
       }
     );
+
+    this.store.dispatch(getBookList());
+
+    // this.store$.select(BookStoreSelectors.getBooks).subscribe(
+    //     books => 
+    //     {
+    //         this.books$ = of(this.books);
+    //     }
+    // );
+
   }
 
   onAdd(bookId: string) {
@@ -41,5 +73,9 @@ export class DummyPage2Component implements OnInit {
  
   onRemove(bookId: string) {
     this.store.dispatch(removeBook({ bookId }));
+  }
+
+  reload() {
+    this.store.dispatch(getBookList());
   }
 }
