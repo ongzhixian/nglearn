7892989157af88edc047e7bacfe4e1896a5f8e4b
Zhixian Ong <zhixian@hotmail.com>
2021-11-18 18:11:00 +0800

    fix tests

--- projects/travel-app/src/app/app.component.spec.ts
+++ projects/travel-app/src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('travel-app app is running!');
+    expect(compiled.querySelector('span')?.textContent).toContain('Travel Recommender');
   });
 });
--- projects/travel-app/src/app/components/dummy-page1/dummy-page1.component.spec.ts
+++ projects/travel-app/src/app/components/dummy-page1/dummy-page1.component.spec.ts
@@ -1,6 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DummyPage1Component } from './dummy-page1.component';
+
+import { AppRoutingModule } from '../../modules/app-routing/app-routing.module';
+import { MaterialModule } from '../../modules/material/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AppComponent } from '../../app.component';
 
 describe('DummyPage1Component', () => {
   let component: DummyPage1Component;
@@ -8,7 +15,15 @@ describe('DummyPage1Component', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DummyPage1Component ]
+      declarations: [ AppComponent, DummyPage1Component ],
+      imports: [
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        FormsModule, 
+        ReactiveFormsModule,
+        MaterialModule,
+        FlexLayoutModule
+      ]
     })
     .compileComponents();
   });
