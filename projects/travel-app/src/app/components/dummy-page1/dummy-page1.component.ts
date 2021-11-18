9fb98cc95f62838495771befa5840d16750e85da
Zhixian Ong <zhixian@hotmail.com>
2021-11-18 14:07:14 +0800

    Working autocomplete

--- projects/travel-app/src/app/components/dummy-page1/dummy-page1.component.ts
+++ projects/travel-app/src/app/components/dummy-page1/dummy-page1.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {Observable, of} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+
+import { TravelLane } from '../../models/TravelLane';
+import TravelLaneJson from '../../../assets/travel-lanes.json';
 
 @Component({
   selector: 'app-dummy-page1',
@@ -7,9 +13,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DummyPage1Component implements OnInit {
 
-  constructor() { }
+  countryInputBox = new FormControl();
+  // options: string[] = ['One', 'Two', 'Three', 'Singapore', 'Switzerland', 'Germany'];
+  filteredOptions: Observable<TravelLane[]>;
+  travelLanes: TravelLane[] = TravelLaneJson;
 
-  ngOnInit(): void {
+
+  constructor() {
+    this.filteredOptions = this.countryInputBox.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value)),
+    );
   }
 
+  ngOnInit(): void {
+
+  }
+
+  private _filter(value: string): TravelLane[] {
+    const filterValue = value.toLowerCase();
+    return this.travelLanes.filter(travelLane => travelLane.country_name.toLowerCase().includes(filterValue));
+  }
 }
