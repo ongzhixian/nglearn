f709f25031b57801d9e98811d40df313916f7d2a
Zhixian Ong <zhixian@hotmail.com>
2021-11-19 11:19:58 +0800

    nesting test

--- projects/travel-app/src/app/components/dummy-page1/dummy-page1.component.spec.ts
+++ projects/travel-app/src/app/components/dummy-page1/dummy-page1.component.spec.ts
@@ -33,53 +33,32 @@ describe('DummyPage1Component', () => {
     expect(component).toBeTruthy();
   });
 
-});
-
-describe('When Singapore is selected', () => {
-  let component: DummyPage1Component;
-  let fixture: ComponentFixture<DummyPage1Component>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AppComponent, DummyPage1Component ],
-      imports: [
-        BrowserAnimationsModule,
-        ReactiveFormsModule,
-        MaterialModule
-      ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DummyPage1Component);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    let userInput:TravelLane = {
-      "id": 199,
-      "country_iso": "SG",
-      "country_name": "Singapore",
-      "lane_name": "STL-Cat4",
-      "filters": {
-          "lane": "STL-Cat4",
-          "fully_vaccinated": false,
-          "age_above_12": false,
-          "required_residential_status": "",
-          "no_pcr": false,
-          "no_art": false,
-          "no_shn": false,
-          "no_sdf": false,
-          "link": "https://safetravel.ica.gov.sg/travel-checklist/category-4",
-          "more_details": ""
-      }
-    };
-
-    component.countryInputBox.setValue(userInput);
-
-    expect(component.selectedTravelLane).toEqual(userInput);
-  });
+  describe("When a TravelLane option is selected ",function () {
+   
+    it('should set TravelLane to be selectedTravelLane', () => {
+      let userInput:TravelLane = {
+        "id": 199,
+        "country_iso": "SG",
+        "country_name": "Singapore",
+        "lane_name": "STL-Cat4",
+        "filters": {
+            "lane": "STL-Cat4",
+            "fully_vaccinated": false,
+            "age_above_12": false,
+            "required_residential_status": "",
+            "no_pcr": false,
+            "no_art": false,
+            "no_shn": false,
+            "no_sdf": false,
+            "link": "https://safetravel.ica.gov.sg/travel-checklist/category-4",
+            "more_details": ""
+        }
+      };
+  
+      component.countryInputBox.setValue(userInput);
+  
+      expect(component.selectedTravelLane).toEqual(userInput);
+    });
+  }); // end describe("When a TravelLane option is selected
 
 });
