fba2f65ae6449c66b9446942e9b0585d08ccdd6d
Zhixian Ong <zhixian@hotmail.com>
2021-11-22 18:06:29 +0800

    revert to original

--- projects/first-lib/src/lib/first-lib.module.ts
+++ projects/first-lib/src/lib/first-lib.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { FirstLibComponent } from './first-lib.component';
-import { DummyPage4Component } from './components/dummy-page4/dummy-page4.component';
-
 
 
 @NgModule({
   declarations: [
-    FirstLibComponent,
-    DummyPage4Component
+    FirstLibComponent
   ],
   imports: [
   ],
