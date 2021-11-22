913d8457b019395646a58014ae0d2c7869a2b4a7
Zhixian Ong <zhixian@hotmail.com>
2021-11-22 15:05:54 +0800

    add resource-not-found routing notes

--- projects/travel-app/src/app/modules/app-routing/app-routing.module.ts
+++ projects/travel-app/src/app/modules/app-routing/app-routing.module.ts
@@ -5,11 +5,20 @@ import { DummyPage1Component } from '../../components/dummy-page1/dummy-page1.co
 import { ResourceNotFoundComponent } from '../../components/resource-not-found/resource-not-found.component';
 
 
-const routes : Routes = [
+const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'page1', component: DummyPage1Component },
-  { path: '**', component: ResourceNotFoundComponent}
+
+  // Resource not found routing using redirection
+  // This means whenever resource is not found, users will be re-directed to: http://localhost:4200/resource-not-found
+  { path: 'resource-not-found', component: ResourceNotFoundComponent },
+  { path: '**', redirectTo: 'resource-not-found' }
+
+  // Note: We could also do away the redirecting with the following, 
+  // it which case, the url bar will display url as entered (example: http://localhost:4200/homeXxx)
+  // Redirection gives a consistent reporting; this gives a accurate experience.
+  // { path: '**', component: ResourceNotFoundComponent }
 ];
 
 @NgModule({
