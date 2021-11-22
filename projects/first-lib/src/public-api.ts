e2c0bee21988528437914f616f91833b5fea2e87
Zhixian Ong <zhixian@hotmail.com>
2021-11-22 18:06:47 +0800

    export dummy-page4

--- projects/first-lib/src/public-api.ts
+++ projects/first-lib/src/public-api.ts
@@ -4,4 +4,5 @@
 
 export * from './lib/first-lib.service';
 export * from './lib/first-lib.component';
+export * from './lib/components/dummy-page4/dummy-page4.component';
 export * from './lib/first-lib.module';
