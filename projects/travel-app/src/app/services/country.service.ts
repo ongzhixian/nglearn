ee3b757977890bad6b7494aa625a04fafe939475
Zhixian Ong <zhixian@hotmail.com>
2021-11-26 18:04:01 +0800

    add country service

---
+++ projects/travel-app/src/app/services/country.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Country } from '../models/Country';
+import { Observable, of } from 'rxjs';
+import { tap, map, catchError } from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CountryService {
+
+    constructor(private http: HttpClient) { }
+
+    getCountries(startsWith: string): Observable<Array<Country>> {
+        return this.http
+            .get<{ items: Country[] }>(`http://localhost:8081/country?startswith=${encodeURIComponent(startsWith)}`)
+            .pipe(
+                tap(_ => console.log(`${_.items.length} items retrieved.`)),
+                map((books) => books.items || []),
+                catchError(this.handleError<Country[]>('Get countries', []))
+            );
+
+    }
+
+
+    private handleError<T>(operation = 'operation', result?: T) {
+        return (error: any): Observable<T> => {
+
+            // TODO: send the error to remote logging infrastructure
+            console.error(error); // log to console instead
+
+            // this.log.add(`${operation} failed: ${error.message}`);
+
+            // Let the app keep running by returning an empty result.
+            return of(result as T);
+        };
+    }
+}
