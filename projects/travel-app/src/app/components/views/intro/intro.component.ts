009b92678416b54063785ab96b6e90c2b2f674a3
Zhixian Ong <zhixian@hotmail.com>
2021-12-02 16:01:58 +0800

    update intro component behaviour

--- projects/travel-app/src/app/components/views/intro/intro.component.ts
+++ projects/travel-app/src/app/components/views/intro/intro.component.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 import { selectQuestion } from '../../../state/travel-app.selectors';
 import { navigateToPage } from '../../../state/travel-app.actions';
 
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -17,48 +17,42 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class IntroComponent implements OnInit {
 
+    question$ : Observable<PolarQuestion> = this.store.select(selectQuestion);
+
     constructor(
         private store: Store<AppState>,
         private route: ActivatedRoute
-    ) { 
+    ) {}
 
-        
-    }
+    ngOnInit(): void {}
 
-    question$ : Observable<PolarQuestion> = this.store.select(selectQuestion);
-
-    ngOnInit(): void {
-        // this.route.url.subscribe(url => {
-        //     console.log(url);
-        // }
-        console.log(this.route.snapshot.url[0].path);
-    }
-
-    onNext(nextQuestion: string) {
-        console.log("Receive event nextQuestin: " + nextQuestion);
-        // this.question = {
-        //     html: 'Do you have a destination in mind?',
-        //     yesRoute: '/page1',
-        //     noRoute: '/home',
-        //     nextEvent: 'ask residency next'
-        // }
-        this.store.dispatch(displayQuestion({
-            question: {
-                html: 'Do you have a destination in mind?',
-                yesRoute: '/page1',
-                noRoute: '/home',
-                nextEvent: 'ask residency next'
-            }
-        }));
-    }
-
-    goToPage1() {
+    goToPath(path: string) {
         // routerLink="/travel-alone"
-        console.log("navigateToPage: %s, %s", this.route.snapshot.url[0].path, '/travel-alone');
-        console.log(this.route.snapshot.toString());
+        // console.log("navigateToPage: %s, %s", this.route.snapshot.url[0].path, '/travel-alone');
+        // console.log(this.route.snapshot.toString());
         this.store.dispatch(navigateToPage({ 
             src: this.route.snapshot.url[0].path,
-            dst: 'travel-alone'
+            dst: path
         }));
     }
+
+    // onNext(nextQuestion: string) {
+    //     // console.log("Receive event nextQuestin: " + nextQuestion);
+    //     // this.question = {
+    //     //     html: 'Do you have a destination in mind?',
+    //     //     yesRoute: '/page1',
+    //     //     noRoute: '/home',
+    //     //     nextEvent: 'ask residency next'
+    //     // }
+    //     this.store.dispatch(displayQuestion({
+    //         question: {
+    //             html: 'Do you have a destination in mind?',
+    //             yesRoute: '/page1',
+    //             noRoute: '/home',
+    //             nextEvent: 'ask residency next'
+    //         }
+    //     }));
+    // }
+
+
 }
