6fd55f35d866f5dd7f91be2cc00e46e3186058e6
Zhixian Ong <zhixian@hotmail.com>
2021-12-02 14:05:14 +0800

    update polar question

--- projects/travel-app/src/app/components/polar-question/polar-question.component.ts
+++ projects/travel-app/src/app/components/polar-question/polar-question.component.ts
@@ -2,20 +2,20 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { PolarQuestion } from '../../models/PolarQuestion';
 
 @Component({
-  selector: 'app-polar-question',
-  templateUrl: './polar-question.component.html',
-  styleUrls: ['./polar-question.component.css']
+    selector: 'app-polar-question',
+    templateUrl: './polar-question.component.html',
+    styleUrls: ['./polar-question.component.css']
 })
 export class PolarQuestionComponent implements OnInit {
 
-  @Input()
-  question!: PolarQuestion;
+    @Input()
+    question!: PolarQuestion;
 
-  @Output() next = new EventEmitter<string>();
+    @Output() next = new EventEmitter<string>();
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
