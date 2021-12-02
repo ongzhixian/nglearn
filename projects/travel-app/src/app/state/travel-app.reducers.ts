1f76e5a3cb8f6e33c39a398b852d93fc8e20442c
Zhixian Ong <zhixian@hotmail.com>
2021-12-02 14:06:05 +0800

    update app reducer

--- projects/travel-app/src/app/state/travel-app.reducers.ts
+++ projects/travel-app/src/app/state/travel-app.reducers.ts
@@ -3,9 +3,16 @@ import { dataReceived } from './travel-info.actions';
 import { displayQuestion } from './travel-app.actions';
 import { PolarQuestion } from '../models/PolarQuestion';
 
+// export const initialState: PolarQuestion = {
+//     html: '<span style=font-size:20pt;>Hey,</span><br/>Would you like to travel?',
+//     yesRoute: '/page1',
+//     noRoute: '/home',
+//     nextEvent: 'next question please'
+// };
+
 export const initialState: PolarQuestion = {
-    html: '<span style=font-size:20pt;>Hey,</span><br/>Would you like to travel?',
-    yesRoute: '/page1',
+    html: 'Do you have a specific country in mind?',
+    yesRoute: '/country-search',
     noRoute: '/home',
     nextEvent: 'next question please'
 };
